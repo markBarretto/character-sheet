d14479c7f4921197a7b0d939edb4df712f6cba2c
Mark Barretto <mark.barretto@gmail.com>
2019-07-15 05:09:06 +0800

    initial scaffolded components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { StatsComponent } from './stats/stats.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { SpellsComponent } from './spells/spells.component';
+import { CantripsComponent } from './cantrips/cantrips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    StatsComponent,
+    EquipmentComponent,
+    SpellsComponent,
+    CantripsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cantrips/cantrips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cantrips',
+  templateUrl: './cantrips.component.html',
+  styleUrls: ['./cantrips.component.scss']
+})
+export class CantripsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
